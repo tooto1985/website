6eb28193ce87e6616c66c2b0f9a82f698935ba3a
tooto1985 <tooto1985@gmail.com>
2014-04-03 15:08:51 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    function getContentHeight() {
+        return $(window).height() - $(".header").height() - $(".footer").height();
+    }
+
+    function jsbinembed(code, version) {
+        $(".content").html("<a class=\"jsbin-embed\" href=\"http://jsbin.com/" + code + "/" + version + "/embed?js,output&height=" + getContentHeight() + "px\">JS Bin</a>");
+    }
+    
+    $(window).resize(function() {
+        $(".content").find(">iframe").css({ "min-height": getContentHeight(), "max-height": getContentHeight() });
+    });
+    
+
+    jsbinembed("xigaf", 4);
+
+});
