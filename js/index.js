75c7484b752cd433e5ac0bd97dc7ab12e161a2b0
tooto1985 <tooto1985@gmail.com>
2014-04-03 16:23:13 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,18 +1,35 @@
-﻿$(function () {
+﻿function getContentHeight() {
+    return $(window).height() - $(".header").height() - $(".footer").height();
+}
 
-    function getContentHeight() {
-        return $(window).height() - $(".header").height() - $(".footer").height();
-    }
+function jsbinembed(code, version) {
+    $(".content").html("<a class=\"jsbin-embed\" href=\"http://jsbin.com/" + code + "/" + version + "/embed?js,output&height=" + getContentHeight() + "px\">JS Bin</a>");
+    jsbinrender();
+}
 
-    function jsbinembed(code, version) {
-        $(".content").html("<a class=\"jsbin-embed\" href=\"http://jsbin.com/" + code + "/" + version + "/embed?js,output&height=" + getContentHeight() + "px\">JS Bin</a>");
-    }
-    
-    $(window).resize(function() {
+$(function () {
+    $(".header,.menu").on("mouseenter mouseleave", function (e) {
+        var $menu = $(".menu");
+        if (e.type === "mouseenter") {
+            $menu.stop(false, false).slideDown("fast");
+        } else {
+            $menu.stop(false, false).slideUp("fast");
+        }
+    });
+    $(window).resize(function () {
         $(".content").find(">iframe").css({ "min-height": getContentHeight(), "max-height": getContentHeight() });
     });
-    
 
+    (function () {
+        $.ajax({
+            url: "data/data.json",
+            dataType: "json",
+            success: function (data) {
+                alert(data.length);
+            }
+        });
+    })(); //menu
+
+    $(".menu").hide();
     jsbinembed("xigaf", 4);
-
 });
