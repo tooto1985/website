291f21805a2801c014def4ad6c9f161280a9d4d9
tooto1985 <tooto1985@gmail.com>
2014-04-07 13:11:35 +0800

    update

--- js/index.js
+++ js/index.js
@@ -31,7 +31,6 @@
             if (data[i].name === $(this).text()) {
                 jsbinembed(data[i].code, data[i].version);
                 location.href = location.href.split("#")[0] + "#" + data[i].name;
-                ga('send', 'pageview', location.href);
             }
         }
     });
@@ -116,10 +115,7 @@
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
         ga('create', 'UA-49667069-1', 'tooto1985.github.io');
         ga('send', 'pageview');
-
-
     })(); //google analytics
 });
